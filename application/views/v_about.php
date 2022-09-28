6246b849129f9e2735f13508030819f0df5caf0f
albadaafza@gmail.com <albadaazfa@gmail.com>
2022-09-28 15:31:01 +0700

    tugas pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<selection>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Afza </li> 
+        <li>Nama Belakang : Albada</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan nurul huda komplek SAR no.A43</li> 
+    </ul> 
+    
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Maliau kabupaten sanggau</li> 
+    </ul> 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>futsal</li> 
+        <li>game online</li> 
+    </ul>
+</selection>
